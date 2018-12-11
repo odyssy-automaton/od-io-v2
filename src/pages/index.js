693f64fbfd7eb55ee7fa8cc5863e3ee0b857aa51
oovg <office@vengist.com>
2018-12-11 14:21:57 -0800

    adds lottie and lottie-control component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
+import LottieControl from '../components/shared/lottie/lottie-control';
+
+import * as heroAnimation from '../img/Intro__Animation--1.json';
+import * as heroAnimation2 from '../img/Intro__Animation--2.json';
 
 export default class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
+        <section className="Hero">
+           <LottieControl
+               animationData={heroAnimation}
+               loop={false}
+               swapAnimationData={heroAnimation2}
+               swapLoop={true} />
+        </section>
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Home</h1>
+              <h1>Home</h1>
             </div>
           </div>
         </section>
