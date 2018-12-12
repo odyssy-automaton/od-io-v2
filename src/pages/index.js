6351399ef9febc29ae33963a7551affb888a22a4
oovg <office@vengist.com>
2018-12-12 13:20:51 -0800

    adds react-reveal and begins implementation. more blocks on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout/Layout';
-import '../styles/index.scss';
+import '../styles/Home.scss';
 import LottieControl from '../components/shared/lottie/LottieControl';
 import OdyssyLogoTypeStandard from '../img/odyssy__logo--type--standard.svg';
 import heroAnimation from '../img/Intro__Animation--1.json';
 import heroAnimation2 from '../img/Intro__Animation--2.json';
-
-console.log(heroAnimation.default);
+import Fade from 'react-reveal/Fade'
 
 export default class IndexPage extends React.Component {
   render() {
@@ -15,10 +14,12 @@ export default class IndexPage extends React.Component {
       <Layout>
         <section className="Hero">
           <div className="Hero__Contents">
+            <Fade cascade wait={10000}>
             <img alt="Odyssy Logo Type" src={OdyssyLogoTypeStandard} />
-            <p>Decentralized<br />
-            Autonomous<br />
-            Organism</p>
+            <p>Decentralized</p>
+            <p>Autonomous</p>
+            <p>Organism</p>
+            </Fade>
           </div>
           <LottieControl
             animationData={heroAnimation}
@@ -34,8 +35,20 @@ export default class IndexPage extends React.Component {
         </section>
         <section className="Block">
           <div className="Block__Contents">
+            <p className="Huge">Proof of Work</p>
+            <Link to="/work">View</Link>
+          </div>
+        </section>
+        <section className="Block">
+          <div className="Block__Contents">
             <p className="Huge"><span className="Weight--500">Odyssy</span> is a decentralized collective delivering full stack Design & Development to the blockchain and beyond.</p>
             <Link to="/services">View our capabilities</Link>
+          </div>
+        </section>
+        <section className="Block">
+          <div className="Block__Contents">
+            <p className="Huge">We are iterating on the future of work right now</p>
+            <Link to="/network">Learn more about our network</Link>
           </div>
         </section>
       </Layout>
