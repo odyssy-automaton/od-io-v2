741e92c1b468bbe7b525fc58df0d75f614a6da8a
oovg <office@vengist.com>
2018-12-13 10:29:55 -0800

    animate OdBackground

--- src/components/shared/od-background/OdBackground.js
+++ src/components/shared/od-background/OdBackground.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 import './OdBackground.scss';
 import OdBackgroundTop from '../../../img/odyssy__background--lines--top.svg';
 import OdBackgroundRight from '../../../img/odyssy__background--lines--right.svg';
@@ -9,11 +10,21 @@ const OdBackground = class extends React.Component {
   render() {
     return (
       <div className="OdBackground">
+        <Fade down>
         <img className="OdBackground--Top" src={OdBackgroundTop} alt="background graphic"/>
+        </Fade>
+        <Fade right>
         <img className="OdBackground--Right" src={OdBackgroundRight} alt="background graphic" />
+        </Fade>
+        <Fade up>
         <img className="OdBackground--Bottom" src={OdBackgroundBottom} alt="background graphic" />
+        </Fade>
+        <Fade left>
         <img className="OdBackground--Left" src={OdBackgroundLeft} alt="background graphic" />
+        </Fade>
       </div>
+
+
     );
   }
 };
--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import Layout from '../../components/layout/Layout';
 import OdBackground from '../../components/shared/od-background/OdBackground';
+import Fade from 'react-reveal/Fade';
 
 export default class ServicesPage extends Component {
   render() {
     return (
       <Layout>
         <section className="PageHeader">
+          <Fade>
           <div className="PageHeader__Contents">
             <h1>The recipe for success calls for equal parts Design, Engineering, and Strategy. Add some Blockchain for spice.</h1>
           </div>
+          </Fade>
           <OdBackground />
         </section>
         <section className="Page">
