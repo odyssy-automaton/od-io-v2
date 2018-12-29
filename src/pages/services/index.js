b839ed7970d8e71438e74954ef3f156b0281b10a
oovg <office@vengist.com>
2018-12-29 10:34:47 -0800

    better layout for network and services

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -17,10 +17,8 @@ export default class ServicesPage extends Component {
           </Fade>
           <OdBackground />
         </section>
-        <section className="Page Services">
-          <div className="Page__Contents">
             <div className="Block">
-              <div>
+              <div className="Block__Contents">
                 <h3>Introduction</h3>
                 <h2>Odyssy is a decentralized collective
                   delivering full stack design and
@@ -30,22 +28,7 @@ export default class ServicesPage extends Component {
               </div>
             </div>
             <div className="Block">
-              <ul>
-                <li><h4>Decentralized</h4><p>Local autonomy for optimal efficiency</p></li>
-                <li><h4>Transparent</h4><p>Information open to all stakeholders</p></li>
-                <li><h4>Collaborative</h4><p>Anti-rivalrous network structure</p></li>
-              </ul>
-              <ul>
-                <li><h4>Trustless</h4><p>Value Creation over Perception</p></li>
-                <li><h4>Balance</h4><p>Balance between centralized and
-                  decentralized</p></li>
-                <li><h4>Iterative</h4><p>Understand, Diverge, Converge, Test,
-                  Prototype, Deploy, Iterate</p></li>
-              </ul>
-            </div>
-
-            <div className="Block">
-              <div>
+              <div className="Block__Contents">
                 <h3>We don’t care about the technology.</h3>
                 <h2>We care about its applications and impact
                   on the human experience.
@@ -53,35 +36,37 @@ export default class ServicesPage extends Component {
               </div>
             </div>
             <div className="Block">
-              <ul>
-                <li><h4>Product Design</h4></li>
-                <li><h4>Strategy</h4></li>
-                <li><h4>Design Thinking</h4></li>
-                <li><h4>Branding</h4></li>
-                <li><h4>UX-UI</h4></li>
-                <li><h4>Iconography</h4></li>
-                <li><h4>Typography</h4></li>
-              </ul>
-              <ul>
-                <li><h4>Front End Development</h4></li>
-                <li><h4>Back End Development</h4></li>
-                <li><h4>Dev Ops</h4></li>
-                <li><h4>R&D</h4></li>
-                <li><h4>Product Management</h4></li>
-                <li><h4>QA & User Testing</h4></li>
-                <li><h4>Web Apps</h4></li>
-              </ul>
-              <ul>
-                <li><h4>Smart Contract Development and Consulting</h4></li>
-                <li><h4>Decentralized Apps (DApps)</h4></li>
-                <li><h4>Blockchain APIs</h4></li>
-                <li><h4>Web3 Interfaces and Toolsets</h4></li>
-                <li><h4>Tokenomics</h4></li>
-              </ul>
+              <div className="Block__Contents">
+                <ul>
+                  <li><h4>Product Design</h4></li>
+                  <li><h4>Strategy</h4></li>
+                  <li><h4>Design Thinking</h4></li>
+                  <li><h4>Branding</h4></li>
+                  <li><h4>UX-UI</h4></li>
+                  <li><h4>Iconography</h4></li>
+                  <li><h4>Typography</h4></li>
+                </ul>
+                <ul>
+                  <li><h4>Front End Development</h4></li>
+                  <li><h4>Back End Development</h4></li>
+                  <li><h4>Dev Ops</h4></li>
+                  <li><h4>R&D</h4></li>
+                  <li><h4>Product Management</h4></li>
+                  <li><h4>QA & User Testing</h4></li>
+                  <li><h4>Web Apps</h4></li>
+                </ul>
+                <ul>
+                  <li><h4>Smart Contract Development and Consulting</h4></li>
+                  <li><h4>Decentralized Apps (DApps)</h4></li>
+                  <li><h4>Blockchain APIs</h4></li>
+                  <li><h4>Web3 Interfaces and Toolsets</h4></li>
+                  <li><h4>Tokenomics</h4></li>
+                </ul>
+              </div>
             </div>
 
             <div className="Block">
-              <div>
+              <div className="Block__Contents">
                 <h3>We’re technology-agnostic and cross-protocol but like to work with</h3>
                 <p>Python, Ruby, Javascript/Node, Django, Rails, React, Angular, Vue, AWS,
 Heroku, IPFS, Ethereum, Solidity.
@@ -89,8 +74,6 @@ Heroku, IPFS, Ethereum, Solidity.
               </div>
             </div>
 
-          </div>
-        </section>
       </Layout>
     );
   }
